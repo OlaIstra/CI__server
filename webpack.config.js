34e76f67aead4828874b29992cb7de1bd85a6d7e
OlaIstra <elhazne@gmail.com>
2020-05-22 10:08:15 +0300

    fixed quotes & added ts-lint-staged

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,52 @@
-const path = require("path");
-var HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
-const isProd = process.env.NODE_ENV === "production";
+const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 
 const filename = (extension) =>
 	isDev ? `bundle.${extension}` : `bundle.[hash].${extension}`;
 
 module.exports = {
-	context: path.resolve(__dirname, "src"),
-	mode: isDev ? "development" : "production",
-	entry: ["./index.tsx"],
+	context: path.resolve(__dirname, 'src'),
+	mode: isDev ? 'development' : 'production',
+	entry: ['./index.tsx'],
 	output: {
-		filename: filename("js"),
-		path: path.resolve(__dirname, "dist"),
+		filename: filename('js'),
+		path: path.resolve(__dirname, 'dist'),
 	},
 	resolve: {
-		extensions: [".tsx", ".ts", ".js"],
+		extensions: ['.tsx', '.ts', '.js'],
 		alias: {
-			"@core": path.resolve(__dirname, "src/core"),
-			"@pages": path.resolve(__dirname, "src/pages"),
-			"@atoms": path.resolve(__dirname, "src/core/atoms"),
+			'@core': path.resolve(__dirname, 'src/core'),
+			'@pages': path.resolve(__dirname, 'src/pages'),
+			'@atoms': path.resolve(__dirname, 'src/core/atoms'),
 		},
 	},
-	devtool: isDev ? "inline-source-map" : false,
+	devtool: isDev ? 'inline-source-map' : false,
 	devServer: {
 		port: 3020,
 		hot: isDev,
 	},
 	optimization: {
 		splitChunks: {
-			chunks: "all",
+			chunks: 'all',
 		},
 	},
 	plugins: [
 		new CleanWebpackPlugin(),
 		new HtmlWebpackPlugin({
-			favicon: path.resolve(__dirname, "src/favicon.ico"),
-			template: "index.html",
+			favicon: path.resolve(__dirname, 'src/favicon.ico'),
+			template: 'index.html',
 			minify: {
 				removeComments: isProd,
 				collapseWhitespace: isProd,
 			},
 		}),
 		new MiniCssExtractPlugin({
-			filename: filename("css"),
+			filename: filename('css'),
 		}),
 	],
 	module: {
@@ -54,15 +54,15 @@ module.exports = {
 			{
 				test: /\.(css)$/,
 				use: [
-					isDev ? "style-loader" : MiniCssExtractPlugin.loader,
-					"css-loader",
+					isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+					'css-loader',
 				],
 			},
 			{
 				test: /\.s[ac]ss$/i,
 				use: [
 					isDev
-						? "style-loader"
+						? 'style-loader'
 						: {
 							loader: MiniCssExtractPlugin.loader,
 							options: {
@@ -70,20 +70,20 @@ module.exports = {
 								reloadAll: true,
 							},
 						},
-					"css-loader",
-					"sass-loader",
+					'css-loader',
+					'sass-loader',
 				],
 			},
 			{
 				test: /\.tsx?$/,
-				use: "babel-loader",
+				use: 'babel-loader',
 				exclude: /node_modules/,
 			},
 			{
 				test: /\.(png|jpe?g|gif|eot|svg|ttf|woff|woff2)$/i,
 				use: [
 					{
-						loader: "url-loader",
+						loader: 'url-loader',
 						options: {
 							limit: 8192,
 						},
