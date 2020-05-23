c2df9785adb3ff8147ee4c61c2751d8f039c32a0
OlaIstra <elhazne@gmail.com>
2020-05-23 12:47:22 +0300

    added @typescript-eslint/no-var-requires

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
 const path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
