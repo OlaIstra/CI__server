c17cf17ada46c6a0eec5ed0ed949815d22bf10ad
OlaIstra <elhazne@gmail.com>
2020-05-25 14:35:00 +0300

    combine routes

--- server/index.ts
+++ server/index.ts
@@ -3,21 +3,26 @@ import axios from 'axios';
 import https from 'https';
 
 //import routes
-import { getSettings } from './routes/getSettings';
-import { getBuilds } from './routes/getBuilds';
-import { getCurrentBuild } from './routes/getCurrentBuild';
-import { getCurrentBuildLog } from './routes/getCurrentBuildLog';
-import { postSettings } from './routes/postSettings';
-import { postBuild } from './routes/postBuild';
+import { getSettings, postSettings } from './routes/getSettings';
+import {
+    getBuilds,
+    getCurrentBuild,
+    getCurrentBuildLog,
+    postBuild,
+} from './routes/getBuilds';
 
 const app = express();
 
 const port = 3050;
-const token = 'zzzz';
+const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6IjI5MTgxNDczIiwiaHR0cDovL3NjaGVtYXMueG1sc29hcC5vcmcvd3MvMjAwNS8wNS9pZGVudGl0eS9jbGFpbXMvbmFtZSI6ImtraXJpayIsInVybjpnaXRodWI6bmFtZSI6IktpcmlsbCBLaGFyaXRvbm92IiwidXJuOmdpdGh1Yjp1cmwiOiJodHRwczovL2FwaS5naXRodWIuY29tL3VzZXJzL2traXJpayIsIm5iZiI6MTU4OTk1ODIzNiwiZXhwIjoxNTkyNTUwMjM2LCJpc3MiOiJTaHJpLUhvbWV3b3JrIiwiYXVkIjoiU2hyaS1Ib21ld29yayJ9.B1KA4PyJyGfxcCwsnoxgyUAibwmiFSGoniFM6YpnJX8';
 const baseURL = 'https://hw.shri.yandex/';
 
 const agent = new https.Agent({ rejectUnauthorized: false });
-const axiosInstance = axios.create({ httpsAgent: agent });
+const axiosInstance = axios.create({
+    httpsAgent: agent,
+    headers: { Authorization: `Bearer ${token}` },
+});
 
 getSettings(app, axiosInstance, baseURL, token);
 
--- server/routes/getSettings.ts
+++ server/routes/getSettings.ts
@@ -1,14 +1,29 @@
 import { Request, Response } from 'express';
+
 import { IRoute } from './interface';
 
 export const getSettings: IRoute = (app, axiosInstance, baseURL, token) => {
     app.get('/api/settings', (req: Request, res: Response): void => {
         axiosInstance
-            .get(`${baseURL}api/conf`, {
-                headers: { Authorization: `Bearer ${token}` },
-            })
+            .get(`${baseURL}api/conf`)
             .then(response => {
                 return res.send(response.data);
+            })
+            .catch(err => console.log(err));
+    });
+};
+
+export const postSettings: IRoute = (app, axiosInstance, baseURL) => {
+    app.post('/api/settings', (req: Request, res: Response): void => {
+        axiosInstance
+            .post(`${baseURL}api/conf`, {
+                repoName: 'string',
+                buildCommand: 'string',
+                mainBranch: 'string',
+                period: 0,
+            })
+            .then(response => {
+                return res.send(`New settings: ${response.config.data}`);
             })
             .catch(err => console.log(err));
     });
