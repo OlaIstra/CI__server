e0b7d06ca722a41c6a8a987259be29ee34f9b99f
OlaIstra <elhazne@gmail.com>
2020-06-01 00:07:29 +0300

    started swagger initialization

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import morgan from 'morgan';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 import router from './components/routes';
 
@@ -11,6 +13,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(morgan('combined'));
+
+app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.use('/api', router);
 
