3c184432291b13eeebb6aa72b902d97081fe4f07
KriK96 <skuf962@yandex.ru>
2020-09-07 11:46:56 +0300

    Fixed webpack config for frontend

--- webpack/webpack.client.js
+++ webpack/webpack.client.js
@@ -1,16 +1,16 @@
-/* eslint-disable no-console */
+// /* eslint-disable no-console */
 const path = require('path');
-const webpack = require('webpack');
+// const webpack = require('webpack');
 const { merge } = require('webpack-merge');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const WebpackBar = require('webpackbar');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const TerserPlugin = require('terser-webpack-plugin');
+// const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+// const WebpackBar = require('webpackbar');
 const { config: dotenvConfig } = require('dotenv');
 
-const { stylesInTsLoader } = require('./styles-in-ts-loader');
+// const { stylesInTsLoader } = require('./styles-in-ts-loader');
 const { client } = require('./webpack.common.config');
-const babelrc = require('../babel.config');
 
 const root = process.cwd();
 
@@ -23,146 +23,147 @@ const DEBUG = JSON.stringify(process.env.DEBUG === 'true');
 console.log(`ENV = ${isDev ? 'development' : 'production'}`);
 console.log(`DEBUG = ${DEBUG}`);
 
-const config = merge(client, {
-    output: {
-        filename: isDev ? 'assets/js/[name].js' : 'assets/js/[name]-[hash:8].js',
-        chunkFilename: isDev ? 'assets/js/[name].js' : 'assets/js/[name]-[chunkhash:8].js',
-    },
+// Сюда уберешь спецефичные для фронта правила из webpack.common.config
+const config = merge(client, {});
 
-    resolve: {
-        modules: [root, path.resolve(root, 'src'), path.resolve(root, 'public'), 'node_modules'],
-    },
+// OLD CONFIG
 
-    plugins: [
-        new HtmlWebpackPlugin({
-            favicon: path.resolve(root, 'src/favicon.ico'),
-            template: 'index.html',
-            minify: {
-                removeComments: isProd,
-                collapseWhitespace: isProd,
-            },
-        }),
-        new MiniCssExtractPlugin({
-            filename: 'assets/css/[name].[contenthash:8].css',
-            chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
-        }),
-        new webpack.ContextReplacementPlugin(/node_modules\/moment\/locale/, /ru/),
-        new webpack.DefinePlugin({ DEBUG, isDev }), // plugin to define global constants TODO take isDev from .env
-        new WebpackBar({ name: 'client', color: 'orange' }),
-    ],
+// const config = merge(client, {
+//     output: {
+//         filename: isDev ? 'assets/js/[name].js' : 'assets/js/[name]-[hash:8].js',
+//         chunkFilename: isDev ? 'assets/js/[name].js' : 'assets/js/[name]-[chunkhash:8].js',
+//     },
 
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                exclude: /\.test\.tsx?$/,
-                use: {
-                    loader: 'babel-loader',
-                    options: babelrc,
-                },
-                include: isDev
-                    ? [path.resolve(root, 'src')]
-                    : [path.resolve(root, 'src'), path.resolve(root, 'node_modules')],
-            },
-            {
-                test: /\.s?css$/,
-                use: [
-                    isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
-                    {
-                        loader: stylesInTsLoader,
-                        query: {
-                            mode: isDev ? 'emit' : 'verify', // WHAT is it
-                        },
-                    },
-                    {
-                        loader: 'css-loader',
-                        options: {
-                            localsConvention: 'camelCaseOnly',
-                            modules: {
-                                mode: 'local',
-                                localIdentName: '[name]__[local]--[hash:base64:5]',
-                            },
-                        },
-                    },
-                    {
-                        loader: 'sass-loader',
-                    },
-                ],
-            },
-        ],
-    },
-    optimization: {
-        minimizer: [
-            new TerserPlugin({
-                cache: true,
-                parallel: true,
-                sourceMap: false,
-                terserOptions: {
-                    mangle: false,
-                    output: {
-                        beautify: true,
-                    },
-                },
-            }),
-        ],
-        splitChunks: {
-            cacheGroups: {
-                commons: {
-                    test: /[\\/]node_modules[\\/].*\.(jsx?|tsx?)/,
-                    name: 'vendor',
-                    chunks: 'all',
-                },
-                styles: {
-                    test: /\.(scss|css)$/,
-                    name: 'styles',
-                    chunks: 'all',
-                },
-            },
-        },
-    },
-});
+//     resolve: {
+//         modules: [root, path.resolve(root, 'src'), path.resolve(root, 'public'), 'node_modules'],
+//     },
 
-if (isDev) {
-    // TODO how to use it
-    config.devServer = {
-        host: process.env.WEBPACK_DEV_HOST,
-        port: process.env.WEBPACK_DEV_PORT,
-        contentBase: __dirname + '/public',
-        writeToDisk: true,
-        proxy: {
-            '/api': process.env.WEBPACK_DEV_API,
-            '/graphql': process.env.WEBPACK_DEV_API,
-        },
-        historyApiFallback: true,
-        disableHostCheck: true,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
-            'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
-        },
-        stats: {
-            assets: true,
-            children: false,
-            chunks: false,
-            cached: true,
-            colors: true,
-            errorDetails: false,
-            errors: true,
-            hash: false,
-            modules: false,
-            publicPath: false,
-            reasons: false,
-            source: false,
-            timings: true,
-            version: false,
-            warnings: true,
-        },
-        watchOptions: {
-            ignored: /node_modules/,
-        },
-    };
-    config.output.publicPath = `/`;
-    config.devtool = false;
-}
+//     plugins: [
+//         new HtmlWebpackPlugin({
+//             favicon: path.resolve(root, 'src/favicon.ico'),
+//             template: 'src/index.html',
+//             minify: {
+//                 removeComments: isProd,
+//                 collapseWhitespace: isProd,
+//             },
+//         }),
+//         new MiniCssExtractPlugin({
+//             filename: 'assets/css/[name].[contenthash:8].css',
+//             chunkFilename: 'assets/css/[name].[contenthash:8].chunk.css',
+//         }),
+//         new webpack.ContextReplacementPlugin(/node_modules\/moment\/locale/, /ru/),
+//         new webpack.DefinePlugin({ DEBUG, isDev }), // plugin to define global constants TODO take isDev from .env
+//         new WebpackBar({ name: 'client', color: 'orange' }),
+//     ],
+
+//     module: {
+//         rules: [
+//             {
+//                 test: /\.tsx?$/,
+//                 exclude: /\.test\.tsx?$/,
+//                 include: isDev
+//                     ? [path.resolve(root, 'src')]
+//                     : [path.resolve(root, 'src'), path.resolve(root, 'node_modules')],
+//             },
+//             {
+//                 test: /\.scss$/,
+//                 use: [
+//                     isDev ? 'style-loader' : MiniCssExtractPlugin.loader,
+//                     // {
+//                     //     loader: stylesInTsLoader,
+//                     //     query: {
+//                     //         mode: isDev ? 'emit' : 'verify', // WHAT is it
+//                     //     },
+//                     // },
+//                     {
+//                         loader: 'css-loader',
+//                         options: {
+//                             localsConvention: 'camelCaseOnly',
+//                             modules: {
+//                                 mode: 'local',
+//                                 localIdentName: '[name]__[local]--[hash:base64:5]',
+//                             },
+//                         },
+//                     },
+//                     {
+//                         loader: 'sass-loader',
+//                     },
+//                 ],
+//             },
+//         ],
+//     },
+//     optimization: {
+//         minimizer: [
+//             new TerserPlugin({
+//                 cache: true,
+//                 parallel: true,
+//                 sourceMap: false,
+//                 terserOptions: {
+//                     mangle: false,
+//                     output: {
+//                         beautify: true,
+//                     },
+//                 },
+//             }),
+//         ],
+//         splitChunks: {
+//             cacheGroups: {
+//                 commons: {
+//                     test: /[\\/]node_modules[\\/].*\.(jsx?|tsx?)/,
+//                     name: 'vendor',
+//                     chunks: 'all',
+//                 },
+//                 styles: {
+//                     test: /\.(scss|css)$/,
+//                     name: 'styles',
+//                     chunks: 'all',
+//                 },
+//             },
+//         },
+//     },
+// });
+
+// if (isDev) {
+//     // TODO how to use it
+//     config.devServer = {
+//         host: process.env.WEBPACK_DEV_HOST,
+//         port: process.env.WEBPACK_DEV_PORT,
+//         contentBase: __dirname + '/public',
+//         writeToDisk: true,
+//         proxy: {
+//             '/api': process.env.WEBPACK_DEV_API,
+//             '/graphql': process.env.WEBPACK_DEV_API,
+//         },
+//         historyApiFallback: true,
+//         disableHostCheck: true,
+//         headers: {
+//             'Access-Control-Allow-Origin': '*',
+//             'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, PATCH, OPTIONS',
+//             'Access-Control-Allow-Headers': 'X-Requested-With, content-type, Authorization',
+//         },
+//         stats: {
+//             assets: true,
+//             children: false,
+//             chunks: false,
+//             cached: true,
+//             colors: true,
+//             errorDetails: false,
+//             errors: true,
+//             hash: false,
+//             modules: false,
+//             publicPath: false,
+//             reasons: false,
+//             source: false,
+//             timings: true,
+//             version: false,
+//             warnings: true,
+//         },
+//         watchOptions: {
+//             ignored: /node_modules/,
+//         },
+//     };
+//     config.output.publicPath = `/`;
+//     config.devtool = false;
+// }
 
 module.exports = config;
--- webpack/webpack.common.config.js
+++ webpack/webpack.common.config.js
@@ -1,11 +1,14 @@
+/* eslint-disable sonarjs/no-duplicate-string */
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const LoadablePlugin = require('@loadable/webpack-plugin');
 const { merge } = require('webpack-merge');
 const nodeExternals = require('webpack-node-externals');
 const { config: dotenvConfig } = require('dotenv');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const { stylesInTsLoader } = require('./styles-in-ts-loader');
+const babelrc = require('../babel.config');
+// const { stylesInTsLoader } = require('./styles-in-ts-loader');
 
 const root = process.cwd();
 dotenvConfig({ path: path.resolve(root, '.env') });
@@ -13,36 +16,121 @@ dotenvConfig({ path: path.resolve(root, '.env') });
 const isProd = process.env.NODE_ENV === 'production';
 const isDev = !isProd;
 
+// OLD
+
+// const common = {
+//     // context: path.resolve(root, 'src'),
+//     mode: isDev ? 'development' : 'production',
+//     bail: !isDev,
+//     output: {
+//         path: path.join(root, 'dist'),
+//         publicPath: '/',
+//     },
+//     resolve: {
+//         mainFields: ['browser', 'main', 'module'], // WHAT is it
+//         extensions: ['.ts', '.tsx', '.js', '.json'],
+//         alias: {
+//             '@core': path.resolve(root, 'src/core'),
+//             '@pages': path.resolve(root, 'src/pages'),
+//             '@atoms': path.resolve(root, 'src/core/atoms'),
+//             '@server': path.resolve(root, 'server/'),
+//         },
+//     },
+//     devtool: isDev ? 'inline-source-map' : false,
+//     optimization: {
+//         splitChunks: {
+//             chunks: 'all',
+//         },
+//     },
+//     module: {
+//         strictExportPresence: true,
+//         rules: [
+//             {
+//                 test: /\.(css)$/,
+//                 /* eslint-disable-next-line sonarjs/no-duplicate-string */
+//                 use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
+//             },
+//             {
+//                 test: /\.scss$/,
+//                 use: [
+//                     // { loader: 'isomorphic-style-loader' }, // WHAT is it
+//                     // {
+//                     //     loader: stylesInTsLoader, // WHAT is it
+//                     //     query: {
+//                     //         mode: isDev ? 'emit' : 'verify', // WHAT is it
+//                     //     },
+//                     // },
+//                     {
+//                         loader: 'css-loader', // WHAT is it
+//                         options: {
+//                             localsConvention: 'camelCaseOnly', // WHAT is it
+//                             modules: {
+//                                 mode: 'local', // WHAT is it
+//                                 localIdentName: '[name]__[local]--[hash:base64:5]', // WHAT is it
+//                             },
+//                         },
+//                     },
+//                     'sass-loader',
+//                 ],
+//             },
+//             {
+//                 test: /\.tsx?$/,
+//                 exclude: /node_modules/,
+//                 use: {
+//                     loader: 'babel-loader',
+//                     options: babelrc,
+//                 },
+//             },
+//             {
+//                 test: /\.(png|jpe?g|gif|eot|svg|ttf|woff|woff2)$/i,
+//                 use: [
+//                     {
+//                         loader: 'url-loader',
+//                         options: {
+//                             limit: 8192,
+//                         },
+//                     },
+//                 ],
+//             },
+//         ],
+//     },
+// };
+
 const common = {
-    context: path.resolve(root, 'src'),
+    // context: path.resolve(__dirname, 'src'),
     mode: isDev ? 'development' : 'production',
-    bail: !isDev,
     output: {
-        path: path.join(root, 'dist'),
-        publicPath: '/',
+        // filename: isDev ? `bundle.js` : `bundle.[hash].js`,
+        // path: path.resolve(__dirname, 'dist'),
     },
     resolve: {
-        mainFields: ['browser', 'main', 'module'], // WHAT is it
-        extensions: ['.ts', '.tsx', '.js', '.json'],
+        extensions: ['.tsx', '.ts', '.js'],
         alias: {
-            '@core': path.resolve(__dirname, 'src/core'),
-            '@pages': path.resolve(__dirname, 'src/pages'),
-            '@atoms': path.resolve(__dirname, 'src/core/atoms'),
-            '@server': path.resolve(__dirname, 'server/'),
+            '@core': path.resolve(root, 'src/core'),
+            '@pages': path.resolve(root, 'src/pages'),
+            '@atoms': path.resolve(root, 'src/core/atoms'),
+            '@server': path.resolve(root, 'server/'),
         },
     },
     devtool: isDev ? 'inline-source-map' : false,
+    devServer: {
+        port: 5000,
+        hot: isDev,
+    },
     optimization: {
         splitChunks: {
             chunks: 'all',
         },
     },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: isDev ? `bundle.css` : `bundle.[hash].css`,
+        }),
+    ],
     module: {
-        strictExportPresence: true,
         rules: [
             {
                 test: /\.(css)$/,
-                /* eslint-disable-next-line sonarjs/no-duplicate-string */
                 use: [isDev ? 'style-loader' : MiniCssExtractPlugin.loader, 'css-loader'],
             },
             {
@@ -63,15 +151,11 @@ const common = {
             },
             {
                 test: /\.tsx?$/,
-                loader: 'babel-loader',
-                exclude: /node_modules/,
-                options: {
-                    presets: [
-                        'react',
-                        'stage-0',
-                        ['env', { targets: { browsers: ['last 2 versions'] } }],
-                    ],
+                use: {
+                    loader: 'babel-loader',
+                    options: babelrc,
                 },
+                exclude: /node_modules/,
             },
             {
                 test: /\.(png|jpe?g|gif|eot|svg|ttf|woff|woff2)$/i,
@@ -106,6 +190,14 @@ const client = merge(common, {
     },
     plugins: [
         new LoadablePlugin(), // WHY there are no other plugins?
+        new HtmlWebpackPlugin({
+            favicon: path.resolve(root, 'src/favicon.ico'),
+            template: path.resolve(root, 'src', 'index.html'),
+            minify: {
+                removeComments: isProd,
+                collapseWhitespace: isProd,
+            },
+        }),
     ],
 });
 
@@ -120,11 +212,11 @@ const server = merge(common, {
     module: {
         rules: [
             {
-                test: /\.s?css$/,
+                test: /\.scss$/,
                 use: [
-                    { loader: 'isomorphic-style-loader' }, // WHAT is it
+                    // { loader: 'isomorphic-style-loader' }, // Это не нужно
                     {
-                        loader: stylesInTsLoader, // WHAT is it
+                        // loader: stylesInTsLoader, // WHAT is it
                         query: {
                             mode: isDev ? 'emit' : 'verify', // WHAT is it
                         },
