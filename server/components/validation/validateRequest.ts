2b19fa2d1cab310a9e28a1f782d56bc263a64f44
OlaIstra <elhazne@gmail.com>
2021-04-05 14:50:41 +0200

    fixup! added express-validator

--- server/components/validation/validateRequest.ts
+++ server/components/validation/validateRequest.ts
@@ -1,18 +1,25 @@
 import { Request, Response, NextFunction } from 'express';
 import { checkSchema, Schema } from 'express-validator';
 
+import { HttpCode } from '@shared/error/httpStatusCodes';
+
 export const validateRequest = (
     func: (req: Request<any>, res: Response, next: NextFunction) => void,
     schema: Schema,
 ) => async (req: Request, res: Response, next: NextFunction) => {
     const validatedRequest = await checkSchema(schema).run(req);
     let isValidRequest = true;
-    validatedRequest.forEach((item: any) => {
+    const errors = validatedRequest.reduce(function(result: any, item: any) {
         if (item.errors.length > 0) {
             isValidRequest = false;
-            res.send({ status: 400, message: `ERROR: ${item.errors[0].msg}` });
+            result.push(item.errors[0].msg);
         }
-    });
+        return result;
+    }, []);
 
-    isValidRequest && func(req, res, next);
+    if (isValidRequest) {
+        func(req, res, next);
+    } else {
+        res.send({ status: HttpCode.BAD_REQUEST, message: `${errors}` });
+    }
 };
