b2e87cc84aaf579d2b3be74843626b7a4803cdf8
Olga_Ibragimova <Olga_Ibragimova@mckinsey.com>
2021-11-14 16:38:11 +0100

    changed tscheck

--- server/components/validation/validateRequest.ts
+++ server/components/validation/validateRequest.ts
@@ -3,27 +3,17 @@ import { checkSchema, Schema } from 'express-validator';
 
 import { HttpCode } from '@shared/error/httpStatusCodes';
 
-interface ValidationErrorItem {
-    errors: {
-        msg: string;
-    }[];
-}
-
-type Middleware = (req: Request, res: Response, next: NextFunction) => void;
-
-export const validateRequest = (func: Middleware, schema: Schema): Middleware => async (
-    req,
-    res,
-    next,
-) => {
-    const validatedRequest = (await checkSchema(schema).run(req)) as ValidationErrorItem[];
-
-    const errors: string[] = validatedRequest.reduce(function(result: string[], item) {
+export const validateRequest = (
+    func: (req: Request<any>, res: Response, next: NextFunction) => void,
+    schema: Schema,
+) => async (req: Request, res: Response, next: NextFunction) => {
+    const validatedRequest = await checkSchema(schema).run(req);
+    const errors: string[] = validatedRequest.reduce(function(result: string[], item: any) {
         if (item.errors.length > 0) {
             return [...result, item.errors[0].msg];
         }
         return result;
-    }, [] as string[]);
+    }, []);
 
     if (errors.length === 0) {
         func(req, res, next);
