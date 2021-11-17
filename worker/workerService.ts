043600bbb7cf457ec9448adb312242809a3d034b
Olga_Ibragimova <Olga_Ibragimova@mckinsey.com>
2021-11-17 21:51:17 +0100

    fix ts-check

--- worker/workerService.ts
+++ worker/workerService.ts
@@ -1,6 +1,6 @@
 export const workerService = {
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
-    runCommand: async jobData => {
+    runCommand: async (jobData: unknown) => {
         //const { id, repoName, buildCommand, commitHash } = jobData;
         //async RepositoryCommandsService.cloneRepo(repoName);
     },
